8b5ef19048a825a220341bc6bed1d9805a65b992
Mehdi EL KOUHEN <mehdi.elkouhen@gmail.com>
2022-03-30 17:00:01 +0200

    wip

---
+++ tests/test-upload.py
@@ -0,0 +1,19 @@
+import requests
+import logging
+
+from aws_requests_auth.boto_utils import BotoAWSRequestsAuth
+
+
+api_base_url = "https://i536xj8kxk.execute-api.eu-west-1.amazonaws.com/live"
+
+
+logging.basicConfig() # Setup basic logging to stdout
+
+auth = BotoAWSRequestsAuth(aws_host='i536xj8kxk.execute-api.eu-west-1.amazonaws.com',
+                           aws_region='eu-west-1',
+                           aws_service='execute-api')
+
+upload_request = requests.post(f"{api_base_url}/presigned-url", data={},
+    auth=auth)    
+
+print (upload_request)
